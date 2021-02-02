2b54747171c67d2221991d25c25d2ca173f3fbb2
Rodrigo Augusto <rodrigoaugusto839@gmail.com>
2021-02-02 11:04:31 -0300

    User entity and Resouce

---
+++ src/main/java/com/educaweb/demo/resouces/UserResouce.java
@@ -0,0 +1,20 @@
+package com.educaweb.demo.resouces;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.educaweb.demo.entities.User;
+
+@RestController
+@RequestMapping(value = "/Users")
+public class UserResouce {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u=new User(1L,"Maria","maria@gmail.com","99999999","12345");
+		return ResponseEntity.ok().body(u);
+	}
+
+}
